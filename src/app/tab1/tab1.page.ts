aaf3dcb8fae82d6ae5143481b13ea482074150c7
Lamine AZINAKOU <agazinakou@gmail.com>
2019-03-26 15:06:30 +0000

    Migrate to ionic 4

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+// import IntroJS
+declare var require: any
+const IntroJs = require("../../../node_modules/intro.js/intro");
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {
+    setTimeout(() => {
+      this.introMethod();      
+    }, 2000);
+  }
+
+  introMethod() {
+    let intro = IntroJs();
+    console.log("inside intro.js");
+    intro.setOptions({
+      steps: [
+        {
+          element: '#step1',
+          intro: 'Logo',
+          position: 'bottom'
+        },
+        {
+          element: '#step2',
+          intro: 'form',
+          position: 'bottom'
+        },
+        {
+          element: '#step3',
+          intro: 'login button',
+          position: 'bottom'
+        }
+      ],
+      showProgress: true,
+      skipLabel: "Annuler",
+      doneLabel: "Commencer",
+      nextLabel: "Suivant",
+      prevLabel: "Précédent",
+      overlayOpacity: "0.8"
+    });
+    intro.start();
+  }
+}
